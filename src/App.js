a51092627be7c59000c1ac3e93a2f57833175550
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2024-05-13 01:32:21 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor (props){
+    super(props);
+    this.displayState =  (this.state.show)? 'flex': 'none';
+  }
+
+  state = {
+    Person: {
+      fullName: "Josué Mongan",
+      bio: "As a student with a passion for computer science, I plan to study robotics at university. I'm currently learning software development at GomyCode.",
+      imgSrc: "./presentation.jpg",
+      profession: "Junior Software Engineer"
+    },
+    show: true
+  };
+
+  changeShowState = () => {
+    (this.state.show)? this.setState({show: false}): this.setState({show: true});
+  }
+
+  render () {
+    this.displayState = (this.state.show) ? 'flex' : 'none';
+
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style = {{display: this.displayState, flexDirection: 'column', justifyContent:'space-around'}}>
+          <img src = {this.state.Person.imgSrc} alt = "presentation" style = {{borderRadius: '45%', width: '300px', height: '250px', margin: 'auto'}}></img>
+          <h1 style = {{margin: '6px'}}>{this.state.Person.fullName}</h1>
+          <h2 style = {{margin: '6px'}}>{this.state.Person.profession}</h2>
+          <p style = {{textAlign: 'justify', textIndent: '40px', margin: '6px'}}>{this.state.Person.bio}</p>
+      </div> 
+      <button onClick = {this.changeShowState} style = {{borderRadius: '7px', padding: '5px', cursor: 'pointer'}}>Changer la vue</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;

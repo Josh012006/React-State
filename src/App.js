e83c3983e0e73f2b4a082ae33970fe54a91607a1
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2024-05-13 04:15:29 +0000

    Adding a counter

--- src/App.js
+++ src/App.js
@@ -14,25 +14,44 @@ class App extends React.Component {
       imgSrc: "./presentation.jpg",
       profession: "Junior Software Engineer"
     },
-    show: true
+    show: true,
+    interval: null,
+    elapsedTime: 0
   };
 
   changeShowState = () => {
     (this.state.show)? this.setState({show: false}): this.setState({show: true});
   }
 
+  componentDidMount () {
+    // Setting an interval that increment the time each second
+    const interval = setInterval(() => {this.setState((prevState) => ({elapsedTime: prevState.elapsedTime + 1}))}, 1000);
+    this.setState({interval});
+  }
+
+  componentWillUnmount () {
+    // The interval is cleared each time the page is reloaded and a new one is started
+    clearInterval(this.state.interval);
+  }
+
+
   render () {
     this.displayState = (this.state.show) ? 'flex' : 'none';
 
     return(
-    <div className="App">
-      <div style = {{display: this.displayState, flexDirection: 'column', justifyContent:'space-around'}}>
-          <img src = {this.state.Person.imgSrc} alt = "presentation" style = {{borderRadius: '45%', width: '300px', height: '250px', margin: 'auto'}}></img>
-          <h1 style = {{margin: '6px'}}>{this.state.Person.fullName}</h1>
-          <h2 style = {{margin: '6px'}}>{this.state.Person.profession}</h2>
-          <p style = {{textAlign: 'justify', textIndent: '40px', margin: '6px'}}>{this.state.Person.bio}</p>
-      </div> 
-      <button onClick = {this.changeShowState} style = {{borderRadius: '7px', padding: '5px', cursor: 'pointer'}}>Changer la vue</button>
+    <div style = {{width: '400px'}}>
+      <div className="App">
+        <div style = {{display: this.displayState, flexDirection: 'column', justifyContent:'space-around'}}>
+            <img src = {this.state.Person.imgSrc} alt = "presentation" style = {{borderRadius: '45%', width: '300px', height: '250px', margin: 'auto'}}></img>
+            <h1 style = {{margin: '6px'}}>{this.state.Person.fullName}</h1>
+            <h2 style = {{margin: '6px'}}>{this.state.Person.profession}</h2>
+            <p style = {{textAlign: 'justify', textIndent: '40px', margin: '6px'}}>{this.state.Person.bio}</p>
+        </div> 
+        <button onClick = {this.changeShowState} style = {{borderRadius: '7px', padding: '5px', cursor: 'pointer'}}>Changer la vue</button>
+      </div>
+      <div className = "intervalWatcher">
+        <label htmlFor = "intervalDisplay">Time elapsed since the component was last mounted:  <br/><br/> <input readOnly id = "intervalDisplay" value = {this.state.elapsedTime} type='text'></input></label>
+      </div>
     </div>
     );
   }
